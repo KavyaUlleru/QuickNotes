b20073e262c2dc8f87746e803d16beadc1363193
KavyaUlleru <kavyaulleru@gmail.com>
2025-10-30 23:38:38 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -21,8 +21,8 @@ mongoose
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() => console.log('✅ MongoDB connected'))
-  .catch((err) => console.log('❌ DB connection error:', err));
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => console.log('DB connection error:', err));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
